e8d3b52fcfdcc0d467824edcaa681f0dd601c10d
Noah Puckett <noah.ingram.puckett@gmail.com>
2019-09-06 11:13:12 -0700

    store!

--- src/store.js
+++ src/store.js
@@ -1,7 +1,11 @@
-import { createStore } from 'redux';
-// import reducer from './reducers';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { promiseMiddleware } from 'promise-middleware-redux';
+import reducer from './reducers';
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default createStore(
-    // reducer, 
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    reducer, 
+    composeEnhancers(
+        applyMiddleware(promiseMiddleware)
+    )
 );
